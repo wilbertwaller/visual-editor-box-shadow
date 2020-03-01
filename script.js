a6b50cc6f2d15beaf10041979b722375c1b872d5
Wilbert Waller <wilbert.waller@gmail.com>
2020-03-01 10:06:58 -1000

    Init commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('input').forEach(input => {
+  input.addEventListener('input', inputChanged);
+});
+
+function inputChanged(e) {
+  document.documentElement.style.setProperty(`--${e.target.name}`, e.target.value);
+}
